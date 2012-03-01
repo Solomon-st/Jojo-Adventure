4bb1b33a84ca79b576e39301f5b4b36c22878bc7
solomon_li <solomon.li@ymail.com>
2012-03-01 20:35:50 +0800

    dada

--- Jojo-Adventure/src/com/sol/jojo/Jojocat.java
+++ Jojo-Adventure/src/com/sol/jojo/Jojocat.java
@@ -1,5 +1,33 @@
 package com.sol.jojo;
 
-public class Jojocat {
+import android.graphics.Canvas;
+import android.graphics.ColorFilter;
+import android.graphics.drawable.Drawable;
+
+public class Jojocat extends Drawable{
+
+    @Override
+    public void draw(Canvas arg0) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public int getOpacity() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void setAlpha(int alpha) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void setColorFilter(ColorFilter cf) {
+        // TODO Auto-generated method stub
+        
+    }
     
 }
